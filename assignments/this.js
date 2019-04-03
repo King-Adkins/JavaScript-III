2cd731329487573c37dd92e4a7624933f161ec13
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-04-03 12:53:17 -0700

    add this answers

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,35 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.new keyword 
+* 2.call, apply, and bind
+* 3.obj.func()
+* 4.is the function called in the global scope. 
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
+    const MyObject = function () {
+        this.name = 'MyObjectName';
+        this.myProperty = 'property';
+};
+
 
 // Principle 2
-
 // code example for Implicit Binding
 
-// Principle 3
+    MyObject.prototype.doSomething = function (action) {
+        console.log(this.name + ' is ' + action + '!');
+    }
 
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+    const obj = new MyObject();
 
+// Principle 4
 // code example for Explicit Binding
+
+    MyObject.call(name, param1, param2);
